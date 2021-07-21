dc0c8d6bd01cfaa386bd54d5c4ec3b6ba5d11214
Xelxeim <xelxeim@gmail.com>
2021-07-21 13:03:12 +0300

    Create api service

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,10 @@
 import './App.scss';
+import apiInteractor from '../../services/api-interactor';
 
 const App = () => {
+
+  const data = apiInteractor.getData("https://city-mobil.ru/api/cars");
+
   return (
     <div className="App">
       
